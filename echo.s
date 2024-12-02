251741df27fea9f50a16467a6daf59bc2eaa1b64
Christophe Thibaut <cthibauttof@gmail.com>
2024-12-02 15:02:51 +0100

    echo example

--- echo.s
+++ echo.s
@@ -1,14 +1,39 @@
-
-loop:
+echo:
+    ldy #$00
+input:
     jsr $FFCC
-    cmp #$2E
-    beq end
-    clc
-    cmp #$30
-    bcc loop
-    cmp #$3A
-    bcs loop
+    cmp #$0A
+    beq end_input
+    sta buffer,y
+    iny
+    bpl input
+end_input:
+    cpy #$00
+    beq end_prog
+    sty length
+    ldy #$00
+print:
+    cpy length
+    beq end_print
+    lda buffer,y
     jsr $FFEE
-    jmp loop
-end:
+    iny
+    jmp print
+end_print:
+    lda #$0D
+    jsr $FFEE
+    lda #$0A
+    jsr $FFEE
+    ldy length
+    bne echo
+end_prog:
     rts
+
+buffer
+
+.dsb 255,0 
+
+length
+
+.byte 0
+
