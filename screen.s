9652d50e71002ff54f06313e5e917459dc9fd04d
Christophe Thibaut <cthibauttof@gmail.com>
2024-12-03 22:11:09 +0100

    playing with screen

--- screen.s
+++ screen.s
@@ -1,17 +1,26 @@
-    lda #$20
+    lda #$00
     ldy #$00
 loop_row:
     ldx #$00
 loop_col:
+    pha
+    cmp #$20
+    bcs print
+    lda #$2E
+print:
     jsr $FFBB
+    pla
+    pha
+    tya
     jsr $FFB0
+    pla
     inc
     cmp #$7F
     bne next
-    lda #$20
+    lda #$00
 next:
     inx
-    cpx #$40
+    cpx #$20
     bne loop_col
     iny
     cpy #$20
